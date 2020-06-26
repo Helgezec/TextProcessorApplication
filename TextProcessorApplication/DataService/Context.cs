4d00f45aa7d01eaa334de4fc848f457f1278e86b
Helgezec <54369812+Helgezec@users.noreply.github.com>
2020-06-26 20:38:13 +0500

    init

---
+++ TextProcessorApplication/DataService/Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using TextProcessorApplication.Models;
+
+namespace TextProcessorApplication.DataService
+{
+    sealed class Context : DbContext
+    {
+        public DbSet<Word> Words { get; set; }
+
+        public Context()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=TextProcessor;Integrated Security=True;");
+
+        }
+    }
+}
---
+++ TextProcessorApplication/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TextProcessorApplication.DataService;
+
+namespace TextProcessorApplication
+{
+    sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                var dictionaryService = new DictionaryService(context);
+                if (args.Length < 2)
+                {
+                    while (true)
+                    {
+                        string prefix = String.Empty;
+                        var key = Console.ReadKey();
+                        if (key.Key == ConsoleKey.Escape || key.Key == ConsoleKey.Enter)
+                            return;
+
+                        prefix += key.KeyChar;
+                        prefix += Console.ReadLine();
+                        if(string.IsNullOrWhiteSpace(prefix))
+                            return;
+                        dictionaryService.Read(prefix);
+                    }
+                }
+
+                else if (args.Length == 2)
+                {
+                    var command = $"{args[0]} {args[1]}";
+                    if (command == "создание словаря")
+                    {
+                        Console.WriteLine("Введите путь к файлу для создания словаря");
+                        dictionaryService.Create(Console.ReadLine());
+                    }
+                    else if (command == "обновление словаря")
+                    {
+                        Console.WriteLine("Введите путь к файлу для обновления словаря");
+                        dictionaryService.Update(Console.ReadLine());
+                    }
+                    else if (command == "очистить словарь")
+                    {
+                        dictionaryService.Delete();
+                    }
+                }
+            }
+        }
+    }
+}
